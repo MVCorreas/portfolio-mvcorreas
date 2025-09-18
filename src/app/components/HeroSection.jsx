e220447af95b4f233224a30b1c06adcf57ff7f42
MVCorreas <correasmv@gmail.com>
2025-09-18 19:23:58 -0300

    style: added responsive position to images

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,26 +6,26 @@ export default function HeroSection() {
       {/* Top Left Image */}
       <Image
         src="/yellow-flowers.jpg"
-        alt="Yellow fabric"
-        width={260}
+        alt="Yellow flowers"
+        width={460}
         height={460}
-        className="absolute top-12 left-4 rounded-xl shadow-lg"
+        className="absolute bottom-120 left-4 rounded-4xl shadow-2xl md:bottom-124 sm:bottom-124"
       />
       {/* Top Right Image */}
       <Image
         src="/yellow-desktop.jpg"
-        alt="Yellow chairs"
+        alt="Yellow desktop"
         width={300}
         height={600}
-        className="absolute top-16 right-12 rounded-xl shadow-lg"
+        className="absolute top-16 right-0 rounded-4xl shadow-2xl md:right-[-48px] sm:right-[-124px]"
       />
       {/* Bottom Left Image */}
       <Image
         src="/yellow-keyboard.jpg"
-        alt="Yellow stairs"
+        alt="Yellow keyboard"
         width={280}
         height={280}
-        className="absolute top-72 left-36 rounded-xl shadow-lg"
+        className="absolute lg:top-64 lg:left-12 rounded-4xl shadow-2xl sm:top-124 sm:left-72"
       />
       <div className="relative z-10 text-center">
         <h1 className="text-6xl font-extrabold mb-4 text-black">
