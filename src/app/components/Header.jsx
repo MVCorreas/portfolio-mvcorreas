bb99571c303448f100c445066388312fe25cc592
MVCorreas <correasmv@gmail.com>
2025-09-18 11:41:10 -0300

    feat: added header, styled basic colors

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="fixed mx-auto top-0 left-0 right-0 z-10 bg-amber-200 text-black bg-opacity-90 h-20">
+      <h1 className="text-2xl font-bold">My Portfolio</h1>
+    </header>
+  );
+}
